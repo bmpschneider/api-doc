460145e69dc3554cdaff66e4d9fe098c26cddfb1
bmpschneider <brunopschneider@gmail.com>
2019-11-20 18:32:30 -0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,29 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app color="dark" dark>     
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/bmpschneider/api-doc"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
+        <span class="d-flex align-center">API DO BRUNO</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
 
   data: () => ({
     //
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <p>{{ nome + path + descricao + metodo}}</p>
+    <v-form ref="form">
+      <v-text-field
+      v-model="nome"
+      :counter="15"
+      :rules="nameRules"
+      outlined
+      label="Name">
+      </v-text-field>
+
+      <v-textarea
+      outlined
+      v-model="descricao"
+      :rules="descricaoRules"
+      label="Descrição">
+      </v-textarea>
+
+      <v-text-field
+      outlined
+      v-model="path"
+      :rules="pathRules"
+      label="Path">
+      </v-text-field>
+
+      <v-combobox
+      outlined
+      v-model="metodo"
+      :items="metodos"
+      :rules="[v => !!v || 'Selecione o método']"
+      label="Método">
+      </v-combobox>      
+
+      <v-btn
+      flat class="success mx-0 mr-4"
+      @click="validate">Validar</v-btn>
+
+      <v-btn
+      flat class="error mx-0 mr-4"
+      @click="reset">Resetar</v-btn>
+
+    </v-form>    
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      nome: "Bruno",
+      descricao: "",
+      path: "",
+      metodo: "",
+      metodos: [
+        'get',
+        'post',
+        'put',
+        'delete'
+      ]
+    }      
+  },
+  methods:{
+    validate() {
+      alert(this.nome)
+    },
+    reset() {
+      alert (this.nome)
+    }
+  }  
 };
 </script>
